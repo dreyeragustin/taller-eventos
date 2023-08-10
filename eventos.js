1449de0e6e09b96619e2aeb9896fc6f0a1279b2b
dreyeragustin <dreyeragustin@gmail.com>
2023-08-09 21:01:28 -0300

    Alerta del Div

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const Div1 = document.getElementById('Div');
+  
+    Div1.addEventListener('click', function() {
+      alert('Hola! Soy el div');
+    });
+  });
